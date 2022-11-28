8639ad52a1de2bbf69f136073d04d4d3214b026a
RajPreet <sahupreetamkumar@gmail.com>
2022-11-28 23:31:02 +0530

    addded functionality with setInterval every sec

--- public/index.js
+++ public/index.js
@@ -1,7 +1,22 @@
-let d = new Date();
+let updateTime = () => {
 
-let us = document.querySelector('#US');
-console.log(us);
-us.innerHTML = d.toLocaleString('en-US',{
-    timeZone: 'America/Los_Angeles'
-})
+    let d = new Date();
+
+    let us = document.querySelector('#US');
+    us.innerHTML = d.toLocaleString('en-US', {
+        timeZone: 'America/Los_Angeles'
+    }).split(',')[1];
+
+    let india = document.querySelector('#india');
+    india.innerHTML = d.toLocaleString('en-US', {
+        timeZone: 'Asia/Kolkata'
+    }).split(',')[1];
+
+    let europe = document.querySelector('#europe');
+    europe.innerHTML = d.toLocaleString('en-US', {
+        timeZone: 'Europe/London'
+    }).split(',')[1];
+
+}
+
+setInterval(updateTime,1000)
